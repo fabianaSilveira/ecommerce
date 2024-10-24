405af4cb744918324ca6bc71f810d2c3adf057c4
Fabiana Cristina da Silveira <fabiana.silveira@tjsc.jus.br>
2024-10-24 18:21:35 -0300

    categorias x produtos

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 
 use \Hcode\Page;
 use  \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
 	$products = Product::listAll();
@@ -14,7 +15,17 @@ $app->get('/', function() {
 
 });
 
+$app->get('/categories/:idcategory', function($idcategory){
+    
+	$category = new Category();
+	$category->get((int)$idcategory);
 
+	$page =new Page();
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
+	]);
+});
 
 
 ?>
